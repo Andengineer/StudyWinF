d6bb0b7d7102b5b53ce53125b36210b028a9fd9e
Andengineer <lamboite10avellaneda@gmail.com>
2024-11-03 23:25:40 -0500

    listar canje

--- StudyWin/src/app/components/canje/listar-canje/listar-canje.component.ts
+++ StudyWin/src/app/components/canje/listar-canje/listar-canje.component.ts
@@ -4,6 +4,8 @@ import { Canje } from '../../../models/Canje';
 import { CanjeService } from '../../../services/canje.service';
 import { MatIconModule } from '@angular/material/icon';
 import { RouterModule } from '@angular/router';
+import { MatSnackBar } from '@angular/material/snack-bar';
+
 
 @Component({
   selector: 'app-listar-canje',
@@ -16,9 +18,8 @@ export class ListarCanjeComponent implements OnInit {
   dataSource:MatTableDataSource<Canje>= new MatTableDataSource()
   displayedColumns:string[]=['c1','c2','c3','c4','c5','eliminar','actualizar']
 
-  constructor(private cS:CanjeService){}
+  constructor(private cS:CanjeService,private snackBar: MatSnackBar){}
   ngOnInit(): void {
-    
       this.cS.list().subscribe(data=>{
         this.dataSource=new MatTableDataSource(data)
       });
@@ -26,11 +27,29 @@ export class ListarCanjeComponent implements OnInit {
         this.dataSource=new MatTableDataSource(data);
       });
   }
-  eliminar(id:number){
-    this.cS.delete(id).subscribe(data=>{
-      this.cS.list().subscribe(data=>{
-        this.cS.setList(data)
-      })
-    })
+  eliminar(id: number): void {
+    this.cS.delete(id).subscribe(
+      () => {
+        // Recargar la lista después de eliminar
+        this.cS.list().subscribe(data => {
+          this.cS.setList(data);
+          this.snackBar.open('Elemento eliminado correctamente.', 'Cerrar', {
+            duration: 3000,
+          });
+        });
+      },
+      error => {
+        // Manejar el error de clave foránea
+        if (error.status === 409) { // Ajusta el código de error si es necesario
+          this.snackBar.open('Elimine el dato foráneo antes de eliminar este registro.', 'Cerrar', {
+            duration: 5000,
+          });
+        } else {
+          this.snackBar.open('Elimine el elemento foraneo de Usuario', 'Cerrar', {
+            duration: 3000,
+          });
+        }
+      }
+    );
   }
 }
