6b47e09d52cdc2199153ba5ceffa502d6a4bb8a9
Andengineer <lamboite10avellaneda@gmail.com>
2024-11-06 14:55:10 -0500

    seguridad

--- StudyWin/src/app/app.component.ts
+++ StudyWin/src/app/app.component.ts
@@ -11,15 +11,34 @@ import { AsociadoComponent } from './components/asociado/asociado.component';
 import { RecompensaComponent } from './components/recompensa/recompensa.component';
 import { PreguntaComponent } from './components/pregunta/pregunta.component';
 import { CuestionarioacademicoComponent } from './components/cuestionarioacademico/cuestionarioacademico.component';
+import { LoginService } from './services/login.service';
+import { CommonModule } from '@angular/common';
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, AsociadoComponent, UsuarioComponent, CursoComponent, CanjeComponent,MatToolbarModule,MatIconModule,MatMenuModule,MatButtonModule,RouterModule,RecompensaComponent,PreguntaComponent,CuestionarioacademicoComponent],
+  imports: [CommonModule,RouterOutlet, AsociadoComponent, UsuarioComponent, CursoComponent, CanjeComponent,MatToolbarModule,MatIconModule,MatMenuModule,MatButtonModule,RouterModule,RecompensaComponent,PreguntaComponent,CuestionarioacademicoComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'StudyWin';
+  role: string = '';
+  constructor(private loginService: LoginService) {}
+
+  cerrar() {
+    sessionStorage.clear();
+  }
+  verificar() {
+    this.role = this.loginService.showRole();
+    return this.loginService.verificar();
+  }
+  isDeveloper() {
+    return this.role === 'DEVELOPER';
+  }
+
+  isclient() {
+    return this.role === 'CLIENT';
+  }
 }
--- StudyWin/src/app/app.routes.ts
+++ StudyWin/src/app/app.routes.ts
@@ -9,21 +9,32 @@ import { AsociadoComponent } from './components/asociado/asociado.component';
 import { PagemainComponent } from './components/mainpage/pagemain/pagemain.component';
 import { CuestionarioacademicoComponent } from './components/cuestionarioacademico/cuestionarioacademico.component';
 import { RecompensaComponent } from './components/recompensa/recompensa.component';
-import { ListarCursoadminComponent } from './components/curso/listar-cursoadmin/listar-cursoadmin.component';
 import { ListarCursoComponent } from './components/curso/listar-curso/listar-curso.component';
 import { UsuarioxcursoComponent } from './components/usuarioxcurso/usuarioxcurso.component';
 import { CreaeditausuarioxcursoComponent } from './components/usuarioxcurso/creaeditausuarioxcurso/creaeditausuarioxcurso.component';
 import { DetallexcanjeComponent } from './components/detallexcanje/detallexcanje.component';
 import { CreaeditaDetallexcanjeComponent } from './components/detallexcanje/creaedita-detallexcanje/creaedita-detallexcanje.component';
 import { PreguntaComponent } from './components/pregunta/pregunta.component';
+import { seguridadGuard } from './guard/seguridad.guard';
+import { LoginComponent } from './components/login/login.component';
 
 export const routes: Routes = [
     {
-        path: 'usuario',component:UsuarioComponent,
-        children:[{
-            path:'nuevo',component:CreaeditausuarioComponent},
+        path: '',
+        redirectTo: 'login',
+        pathMatch: 'full',
+    },
+    {
+        path: 'login',
+        component: LoginComponent,
+    },
+    {
+        path: 'usuario', component: UsuarioComponent,
+        children: [{
+            path: 'nuevo', component: CreaeditausuarioComponent
+        },
         {
-            path:'ediciones/:id',component:CreaeditausuarioComponent
+            path: 'ediciones/:id', component: CreaeditausuarioComponent
         }]
     },
     {
@@ -31,51 +42,54 @@ export const routes: Routes = [
         children: [
             { path: 'nuevo', component: CreaeditacanjeComponent },// Ruta para crear/editar un canje
             {
-                path:'ediciones/:id',component:CreaeditacanjeComponent
+                path: 'ediciones/:id', component: CreaeditacanjeComponent
             }]
     },
     {
-        path: 'curso', component: ListarCursoComponent
-        
+        path: 'curso', component: ListarCursoComponent,
+        canActivate: [seguridadGuard]
+
     },
     {
-        path: 'asociado', component:AsociadoComponent, // Ruta para listar los asociados
+        path: 'asociado', component: AsociadoComponent, // Ruta para listar los asociados
     },
     {
-        path: 'recompensas', component:RecompensaComponent, // Ruta para listar las recompensas
+        path: 'recompensas', component: RecompensaComponent, // Ruta para listar las recompensas
     },
     {
-        path: 'cuestionario', component:CuestionarioacademicoComponent, // Ruta para listar los cuestionarios
+        path: 'cuestionario', component: CuestionarioacademicoComponent, // Ruta para listar los cuestionarios
     },
     {
-        path: 'pregunta', component:PreguntaComponent, // Ruta para listar las preguntas
+        path: 'pregunta', component: PreguntaComponent, // Ruta para listar las preguntas
     },
     {
-        path: '', component:PagemainComponent, // Ruta para listar el mainpage
+        path: 'home', component: PagemainComponent, // Ruta para listar el mainpage
     },
     {
-        path: 'cursoadmin', component:CursoComponent,
-        children:[
-            {path:'ediciones/:id',component:CreaeditacursoComponent},// Ruta para listar los cursos vista admin
-            {path: 'nuevo', component: CreaeditacursoComponent }
-        ] 
+        path: 'cursoadmin', component: CursoComponent,
+        children: [
+            { path: 'ediciones/:id', component: CreaeditacursoComponent },// Ruta para listar los cursos vista admin
+            { path: 'nuevo', component: CreaeditacursoComponent }
+        ]
     },
     {
-        path: 'usuarioxcurso',component:UsuarioxcursoComponent,
-        children:[{
-            path:'nuevo',component:CreaeditausuarioxcursoComponent},
+        path: 'usuarioxcurso', component: UsuarioxcursoComponent,
+        children: [{
+            path: 'nuevo', component: CreaeditausuarioxcursoComponent
+        },
         {
-            path:'ediciones/:id',component:CreaeditausuarioxcursoComponent
+            path: 'ediciones/:id', component: CreaeditausuarioxcursoComponent
         }]
     },
     {
-        path: 'detalle-canje',component:DetallexcanjeComponent,
-        children:[{
-            path:'nuevo',component:CreaeditaDetallexcanjeComponent},
+        path: 'detalle-canje', component: DetallexcanjeComponent,
+        children: [{
+            path: 'nuevo', component: CreaeditaDetallexcanjeComponent
+        },
         {
-            path:'ediciones/:id',component:CreaeditaDetallexcanjeComponent
+            path: 'ediciones/:id', component: CreaeditaDetallexcanjeComponent
         }]
     }
 ]
-;
+    ;
 
