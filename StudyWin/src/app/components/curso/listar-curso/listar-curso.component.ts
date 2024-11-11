fe38bbaa3ba8551b5cded0255d737501d16aabd9
Andengineer <lamboite10avellaneda@gmail.com>
2024-11-11 09:36:43 -0500

    validacion en el boton comenzar

--- StudyWin/src/app/components/curso/listar-curso/listar-curso.component.ts
+++ StudyWin/src/app/components/curso/listar-curso/listar-curso.component.ts
@@ -7,6 +7,7 @@ import { LoginService } from '../../../services/login.service';
 import { UsuarioXCursoService } from '../../../services/usuario-xcurso.service';
 import { UsuarioXCurso } from '../../../models/UsuarioXCurso';
 import { RouterModule } from '@angular/router';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 
 @Component({
@@ -22,11 +23,29 @@ export class ListarCursoComponent implements OnInit {
   identcurso:number=0
   showTitle: boolean = true; // Controla la visibilidad del título
   role: string = '';
-  constructor(private cS: CursoService, private ls: LoginService, private uxcS: UsuarioXCursoService) { }
-  Comenzar(idcurso: number) {
-    this.usXcurso.curso.id_curso = idcurso
-    this.usXcurso.usuario.id_usuario = this.ls.getId()
-    this.uxcS.insert(this.usXcurso).subscribe(d => { })
+  constructor(private cS: CursoService, private ls: LoginService, private uxcS: UsuarioXCursoService,private snackBar: MatSnackBar) { }
+  Comenzar(idcurso: number) {//
+    const userId = this.ls.getId();
+    // Verificar si el usuario ya tiene registrado el curso
+    this.uxcS.list().subscribe(data => {
+      const userCourses = data.filter((element: UsuarioXCurso) =>
+        element.usuario.id_usuario === userId && element.curso.id_curso === idcurso
+      );
+
+      if (userCourses.length > 0) {
+        // Mostrar mensaje de error si el curso ya está inscrito para el usuario
+        this.snackBar.open('Continuando curso', 'Cerrar', {
+          duration: 3000,
+        });
+      } else {
+        // Asociar el curso si no está registrado
+        this.usXcurso.curso.id_curso = idcurso;
+        this.usXcurso.usuario.id_usuario = userId;
+        this.uxcS.insert(this.usXcurso).subscribe(d => {
+          
+        });
+      }
+    });
   }
   isclient() {
     this.role = this.ls.showRole();
