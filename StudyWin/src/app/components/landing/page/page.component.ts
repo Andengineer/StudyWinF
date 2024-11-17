2269a4d335c343c3f36a9b3b5ae46072b3284105
Anderson Avelllaneda <lamboite10avellaneda@gmail.com>
2024-11-17 15:09:54 -0500

    LANDING

--- StudyWin/src/app/components/landing/page/page.component.ts
+++ StudyWin/src/app/components/landing/page/page.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-page',
   standalone: true,
-  imports: [],
+  imports: [RouterModule],
   templateUrl: './page.component.html',
   styleUrl: './page.component.css'
 })
